669f9b1a3a04006840df7b1ec020df985d2ffc9e
SlawekLysakowski <slawku1@gmail.com>
2023-12-08 22:07:12 +0100

    Backend optimization

---
+++ backend/middleware/file.js
@@ -0,0 +1,25 @@
+const multer = require("multer");
+
+const MIMI_TYPE_MAP = {
+  'image/png': 'png',
+  'image/jpeg': 'jpg',
+  'image/jpg': 'jpg'
+};
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const isValid = MIMI_TYPE_MAP[file.mimetype];
+    let error = new Error('Invald mime type');
+    if (isValid) {
+      error = null
+    }
+    cb(error, 'backend/images')
+  },
+  filename: (req, file, cb) => {
+    const name = file.originalname.toLowerCase().split(' ').join('-');
+    const ext = MIMI_TYPE_MAP[file.mimetype];
+    cb(null, name + '-' + Date.now() + '.' + ext)
+  }
+});
+
+module.exports = multer({storage: fileStorage}).single('image');
