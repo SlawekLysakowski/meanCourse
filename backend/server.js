829f850d6ccf17d26ae83d33fad72d9f1327d035
SlawekLysakowski <slawku1@gmail.com>
2023-12-17 20:57:52 +0100

    first deployment setup

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
-const app = require('./backend/app');
 const debug = require('debug')('node-angular');
 const http = require('http');
+
+const app = require('./app');
 
 const normalizePort = (val) => {
   var port = parseInt(val, 10);
